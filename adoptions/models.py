1e9a3c53481f752a934c71f533361c7f717d6e09
dhlaltoo <dhlaltoo@gmail.com>
2018-04-25 19:59:50 -0300

    Fixed spelling errors

--- adoptions/models.py
+++ adoptions/models.py
@@ -5,7 +5,7 @@ class Pet(models.Model):
     SEX_CHOICES = [('M', 'Male',), ('F', 'Female')]
 
     name = models.CharField(max_length=100)
-    submiter = models.CharField(max_length=100)
+    submitter = models.CharField(max_length=100)
     species = models.CharField(max_length=30)
     breed = models.CharField(max_length=30, blank=True) # breed may not be known
     description = models.TextField()
