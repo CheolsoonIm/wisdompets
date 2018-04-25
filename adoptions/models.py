fca492fe9cebff6211e51c4d2ba4d34805cc7f97
dhlaltoo <dhlaltoo@gmail.com>
2018-04-25 14:00:12 -0300

    Added model classes

--- adoptions/models.py
+++ adoptions/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Pet(models.Model):
+
+    SEX_CHOICES = [('M', 'Male',), 'F', 'Female']
+
+    name = models.CharField(max_length=100)
+    submiter = models.CharField(max_length=100)
+    species = models.CharField(max_length=30)
+    breed = models.CharField(max_length=30, blank=True) # breed may not be known
+    description = models.TextField()
+    sex = models.CharField(choices=SEX_CHOICES, max_length=1, blank=True) # choices attribute uses constant (pets may be unable to be sexed)
+    submission_date = models.DateTimeField()
+    age = models.IntegerField(null=True) # in case pets are rescued with unknown age (blank int = 0, so use null=True instead)
+    vaccineations = models.ManyToManyField('Vaccine', blank=True) # many to many model to Vaccine
+
+class Vaccine(models.Model):
+    name = models.CharField(max_length=50)    
